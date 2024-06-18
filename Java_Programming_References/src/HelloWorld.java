359a81b0072ee607abdc9042292b7adbe3f0483e
thoophoms <thoophom.su@gmail.com>
2024-06-17 23:58:30 -0400

    Add Java Programming References into GitHub

--- Java_Programming_References/src/HelloWorld.java
+++ Java_Programming_References/src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, world");
+        System.out.println();
+        System.out.println("Hello, Tom");
     }
+
 }
